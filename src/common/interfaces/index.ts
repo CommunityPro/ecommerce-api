b559e9f51a54c9fbee3e5890b7c0f1960ac6d986
Samson Okunola <smsnmicheal@gmail.com>
2023-09-15 18:57:24 +0100

    setup commit

---
+++ src/common/interfaces/index.ts
@@ -0,0 +1,5 @@
+import { Document } from "mongoose"
+
+export interface Category extends Document {}
+
+export interface Product extends Document {}
---
+++ src/public/js/index.js
@@ -0,0 +1,3 @@
+const root = document.getElementById("root")
+
+root.innerText = "CommunityPro"
