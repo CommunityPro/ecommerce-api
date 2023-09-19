ea22a43409376d49e73133fc33e2e3d3a45ef41c
sparrowsl <benthorpe.dev@gmail.com>
2023-09-19 11:11:24 +0000

    feat: create products and category interface

--- src/common/interfaces/index.ts
+++ src/common/interfaces/index.ts
@@ -1,1 +1,15 @@
-export {}
+interface Category {
+	id?: number
+	name: string
+}
+
+interface Product {
+	id?: number
+	name: string
+	price: number
+	description: string
+	category: Category
+	image: string
+}
+
+export { Category, Product }
