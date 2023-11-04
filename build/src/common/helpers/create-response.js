f214152e6c7f40b9fd570ccab99173fa048f0e1e
Samson Okunola <smsnmicheal@gmail.com>
2023-11-04 08:44:32 +0100

    fixed build output in .gitignore

--- build/src/common/helpers/create-response.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const createResponse = (message, data = [], status = "success") => (res, code) => {
-    return res.status(code).json({ code, status, message, data });
-};
-exports.default = createResponse;
