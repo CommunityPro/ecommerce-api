7eee8b2e3d9d3f20b3b84fd975696dd5c20cd1fc
Samson Okunola <smsnmicheal@gmail.com>
2024-07-18 22:33:39 +0100

    chore: updated scripts

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,8 @@ const server = http.createServer(app)
 mongoose.connect(KEYS.mongo_uri)
 mongoose.set("strictQuery", false)
 const db = mongoose.connection
-db.once("open", () => console.log("Connected to MongoDB"))
+db.once("open", () => console.log("database connection is established"))
 db.on("error", console.error.bind(console, "connection error: "))
 
 const port = process.env.PORT || KEYS.port
-server.listen(port, () => console.log(`App is running on port ${port}`))
+server.listen(port, () => console.log(`app is running on http://localhost:${port}`))
